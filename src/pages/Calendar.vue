a718e405ab6e1dc60e16ba278bea91f91956b962
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-06 13:19:53 +0200

    feat: pages added

---
+++ src/pages/Calendar.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-date v-model="date" />
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { date } from "quasar";
+const timeStamp = Date.now();
+const formattedDate = date.formatDate(timeStamp, "YYYY/MM/DD");
+export default {
+  setup() {
+    return {
+      date: ref(formattedDate),
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <q-markup-table separator="cell">
+      <thead>
+        <tr>
+          <th class="text-left text-weight-bolder">{{ formattedDate }}</th>
+          <th class="text-center text-weight-bolder" style="width: 100%">
+            Tasks
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Frozen Yogurt</td>
+          <td>159</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+    <q-separator color="orange" inset />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { date } from "quasar";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+  setup() {
+    const timeStamp = Date.now();
+    const formattedDate = date.formatDate(timeStamp, "dddd, D MMMM YYYY");
+    return { formattedDate };
+  },
+});
 </script>
