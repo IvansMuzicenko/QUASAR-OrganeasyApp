8183ea7851935425a74da08f9ca71961ebb84fd7
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-15 16:11:08 +0200

    feat: firebase init

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVfbdakVWhg_Sh60yailj4njpQ-Gky-ZQ",
+  authDomain: "quasar-todo-552da.firebaseapp.com",
+  databaseURL:
+    "https://quasar-todo-552da-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "quasar-todo-552da",
+  storageBucket: "quasar-todo-552da.appspot.com",
+  messagingSenderId: "176091632959",
+  appId: "1:176091632959:web:0de3b098f40daa8160af8a",
+  measurementId: "G-2D8RRSSCMD",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
