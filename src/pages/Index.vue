f8680821c6a81f1efbe653616f3e6329b043d81d
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-18 10:28:26 +0200

    feat: sorting by date

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,7 @@
 <template>
   <q-page>
+    <q-btn @click="previousDay()">Previous</q-btn>
+    <q-btn @click="nextDay()">Next</q-btn>
     <q-markup-table separator="cell">
       <thead>
         <tr>
@@ -10,8 +12,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="task in tasks" :key="task">
-          <td>{{ task["time"] }}</td>
+        <tr v-for="task in dayTasks" :key="task">
+          <td>{{ task["time"].slice(task["time"].indexOf(" ")) }}</td>
           <td>{{ task["title"] }}</td>
         </tr>
       </tbody>
@@ -28,14 +30,40 @@ export default {
     tasks() {
       return this.$store.getters["users/userData"].tasks;
     },
+    dayTasks() {
+      const tasks = this.$store.getters["users/userData"].tasks;
+      let dayTasks = {};
+      let i = 0;
+      for (const task in tasks) {
+        i++;
+        if (
+          tasks[task].time.slice(0, tasks[task].time.indexOf(" ")) ==
+          this.formattedDate.slice(this.formattedDate.indexOf(" ") + 1)
+        ) {
+          dayTasks[task] = tasks[task];
+        }
+      }
+      return dayTasks;
+    },
   },
 
   data() {
     const timeStamp = Date.now();
-    const formattedDate = date.formatDate(timeStamp, "dddd, D MMMM YYYY");
+    const formattedDate = date.formatDate(timeStamp, "dddd, DD/MM/YYYY");
     return {
+      timeStamp,
       formattedDate,
     };
+  },
+  methods: {
+    nextDay() {
+      this.timeStamp += 86400000;
+      this.formattedDate = date.formatDate(this.timeStamp, "dddd, DD/MM/YYYY");
+    },
+    previousDay() {
+      this.timeStamp -= 86400000;
+      this.formattedDate = date.formatDate(this.timeStamp, "dddd, DD/MM/YYYY");
+    },
   },
 };
 </script>
