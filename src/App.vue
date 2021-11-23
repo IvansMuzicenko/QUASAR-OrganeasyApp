697ea0060760575982783b46db61a66c0ebb8e11
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-23 15:27:55 +0200

    refactor: format code

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ import {
   // orderByChild,
 } from "firebase/database";
 export default {
-  created() {
+  beforeCreate() {
     const user = this.$q.localStorage.getItem("user");
     if (user) {
       this.$store.dispatch("users/setUser", user);
 
       const db = getDatabase();
-      const dbRef = ref(db, user.userId); //query(ref(db, user.userId), orderByChild("time")) !sort not working
+      const dbRef = ref(db, user.userId);
       onValue(dbRef, (snapshot) => {
         const data = snapshot.val();
         this.$store.dispatch("users/setUserData", data);
