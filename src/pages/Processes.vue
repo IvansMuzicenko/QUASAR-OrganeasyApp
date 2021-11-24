c7d300cff9565362f1cedd5019869e91833e9c28
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-24 17:23:04 +0200

    feat: processes show

--- src/pages/Processes.vue
+++ src/pages/Processes.vue
@@ -1,9 +1,32 @@
 <template>
-  <q-page> </q-page>
+  <q-page>
+    <q-markup-table separator="cell">
+      <!-- <thead>
+        <tr>
+          <th class="text-left text-weight-bolder">{{ formattedDate }}</th>
+          <th class="text-center text-weight-bolder" style="width: 100%">
+            Tasks
+          </th>
+        </tr>
+      </thead> -->
+      <tbody>
+        <tr v-for="(process, index) in processes" :key="index">
+          <td>{{ process["title"] }}</td>
+          <td>{{ process["time"] }}</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    processes() {
+      return this.$store.getters["users/processes"];
+    },
+  },
+};
 </script>
 
 <style></style>
