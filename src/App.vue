c3d725717c224dc7cb42f4efc3cb218940ba3796
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-11-30 12:21:46 +0200

    feat: loader added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <router-view />
+  <router-view v-if="!loading" />
+  <q-inner-loading
+    v-else
+    :showing="loading"
+    label="Please wait..."
+    label-class="text-teal"
+    label-style="font-size: 1.1em"
+  />
 </template>
 
 <script>
@@ -13,6 +20,7 @@ export default {
       const db = getDatabase();
       const dbRef = ref(db, user.userId);
       onValue(dbRef, (snapshot) => {
+        this.loading = false;
         const data = snapshot.val();
         this.$store.dispatch("users/setUserData", data);
       });
@@ -22,6 +30,11 @@ export default {
     if (!this.$store.getters["users/email"]) {
       this.$router.push("/auth");
     }
+  },
+  data() {
+    return {
+      loading: true,
+    };
   },
 };
 </script>
