7be1f56292412d976b1403e4c18179898d729fdd
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-12-01 10:07:26 +0200

    feat: empty list adding button

--- src/pages/Notes.vue
+++ src/pages/Notes.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page>
     <p class="text-h6 text-center">Notes</p>
+    <div v-if="!Object.keys(notes).length">
+      <p>You have not notes!</p>
+      <q-btn color="secondary" @click="addNote()">Add note</q-btn>
+    </div>
     <q-list separator bordered>
       <q-item
         v-for="(note, index) in notes"
@@ -15,10 +19,18 @@
 </template>
 
 <script>
+import addNoteForm from "components/addNoteForm.vue";
 export default {
   computed: {
     notes() {
       return this.$store.getters["users/notes"];
+    },
+  },
+  methods: {
+    addNote() {
+      this.$q.dialog({
+        component: addNoteForm,
+      });
     },
   },
 };
