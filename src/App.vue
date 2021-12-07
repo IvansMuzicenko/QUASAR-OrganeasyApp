c27a7a93b62354c99c8ba3ef5b6d14ae7d30d37f
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-12-07 16:23:06 +0200

    feat: capacitor local notifications

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,19 @@
 
 <script>
 import { getDatabase, ref, onValue } from 'firebase/database'
+import { date } from 'quasar'
+// import capacitor from './capacitor.js'
 export default {
   data() {
     return {
       loading: true
     }
   },
+  // computed: {
+  //   capacitor() {
+  //     return this.$q.platform.is.capacitor
+  //   }
+  // },
   beforeMount() {
     const user = this.$q.localStorage.getItem('user')
     if (user) {
@@ -28,10 +35,112 @@ export default {
         this.loading = false
         const data = snapshot.val()
         this.$store.dispatch('users/setUserData', data)
+        this.capacitor()
       })
     } else {
       this.$router.push('/auth')
       this.loading = false
+    }
+  },
+  methods: {
+    async capacitor() {
+      if (this.$q.platform.is.capacitor) {
+        await this.$q.capacitor.Plugins.LocalNotifications.requestPermissions()
+        const tasks = await this.$store.getters['users/tasks']
+        let notificationList = []
+        for (const tasksDate of Object.keys(tasks)) {
+          for (const task of tasks[tasksDate]) {
+            if (task.notifications) {
+              for (const notification of task.notifications) {
+                let time =
+                  notification.notificationPointModel == 'start time'
+                    ? task.time
+                    : task.endingTime
+                    ? task.endingTime
+                    : task.time
+                if (notification.notificationPeriodModel == 'before') {
+                  time = date.subtractFromDate(
+                    date.extractDate(time, 'DD-MM-YYYY HH:mm'),
+                    {
+                      months:
+                        notification.notificationTimeTypeModel == 'months'
+                          ? notification.notificationTimeValuesModel
+                          : 0,
+                      days:
+                        notification.notificationTimeTypeModel == 'days'
+                          ? notification.notificationTimeValuesModel
+                          : 0,
+                      hours:
+                        notification.notificationTimeTypeModel == 'hours'
+                          ? notification.notificationTimeValuesModel
+                          : notification.notificationTimeTypeModel == 'weeks'
+                          ? notification.notificationTimeValuesModel * 7
+                          : 0,
+                      minutes:
+                        notification.notificationTimeTypeModel == 'minutes'
+                          ? notification.notificationTimeValuesModel
+                          : 0
+                    }
+                  )
+                } else {
+                  time = date.addToDate(
+                    date.extractDate(time, 'DD-MM-YYYY HH:mm'),
+                    {
+                      months:
+                        notification.notificationTimeTypeModel == 'months'
+                          ? notification.notificationTimeValuesModel
+                          : 0,
+                      days:
+                        notification.notificationTimeTypeModel == 'days'
+                          ? notification.notificationTimeValuesModel
+                          : 0,
+                      hours:
+                        notification.notificationTimeTypeModel == 'hours'
+                          ? notification.notificationTimeValuesModel
+                          : notification.notificationTimeTypeModel == 'weeks'
+                          ? notification.notificationTimeValuesModel * 7
+                          : 0,
+                      minutes:
+                        notification.notificationTimeTypeModel == 'minutes'
+                          ? notification.notificationTimeValuesModel
+                          : 0
+                    }
+                  )
+                }
+                console.log(time)
+
+                if (
+                  !notificationList.some(
+                    (element) => element.id === date.formatDate(time, 'x')
+                  )
+                ) {
+                  notificationList.push({
+                    title: task.title,
+                    body: task.title,
+                    id: date.formatDate(time, 'x') / 1000,
+                    schedule: { at: time },
+                    allowWhileIdle: true,
+                    sound: null,
+                    attachments: null,
+                    actionTypeId: '',
+                    extra: null
+                  })
+                }
+              }
+            }
+          }
+        }
+
+        const notifs =
+          await this.$q.capacitor.Plugins.LocalNotifications.schedule({
+            notifications: notificationList
+          })
+        console.log(notifs)
+
+        // console.log(
+        //   await this.$q.capacitor.Plugins.LocalNotifications.getPending()
+        // )
+      }
     }
   }
 }
