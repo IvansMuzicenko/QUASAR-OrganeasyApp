c8110f8d6aad6de9723e881625318c160831954d
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-12-09 10:55:45 +0200

    feat: notifications action buttons

--- src/store/notification/index.js
+++ src/store/notification/index.js
@@ -13,13 +13,75 @@ const mutations = {
 }
 
 const actions = {
-  setCapacitor({ commit }, capacitor) {
+  async setCapacitor({ commit }, capacitor) {
     commit('SET_CAPACITOR', capacitor)
   },
 
-  async addNotifications({ getters, rootGetters }) {
+  async addNotifications({ getters, rootGetters, dispatch }) {
     const capacitor = getters.capacitor
     await capacitor.Plugins.LocalNotifications.requestPermissions()
+    capacitor.Plugins.LocalNotifications.registerActionTypes({
+      types: [
+        {
+          id: 'TASK_NOTIFICATION',
+          actions: [
+            {
+              id: 'view',
+              title: 'Show'
+            },
+            {
+              id: 'progress',
+              title: 'Done'
+            },
+            {
+              id: 'dismiss',
+              title: 'Dismiss',
+              destructive: true
+            }
+          ]
+        },
+        {
+          id: 'CONTINUOUS_TASK_NOTIFICATION',
+          actions: [
+            {
+              id: 'view',
+              title: 'Show'
+            },
+            {
+              id: 'start',
+              title: 'Start'
+            },
+            {
+              id: 'progress',
+              title: 'Done'
+            },
+            {
+              id: 'dismiss',
+              title: 'Dismiss',
+              destructive: true
+            }
+          ]
+        },
+        {
+          id: 'DAILY_CHECK',
+          actions: [
+            {
+              id: 'view',
+              title: 'Show'
+            },
+            {
+              id: 'transfer',
+              title: 'Transfer'
+            },
+            {
+              id: 'dismiss',
+              title: 'Dismiss',
+              destructive: true
+            }
+          ]
+        }
+      ]
+    })
     const tasks = rootGetters['users/tasks']
     let notificationList = []
     for (const tasksDate of Object.keys(tasks)) {
@@ -82,20 +144,31 @@ const actions = {
                   }
                 )
               }
+              const pendetNotifs =
+                await capacitor.Plugins.LocalNotifications.getPending()
+
+              console.log(pendetNotifs)
+
               if (
-                !notificationList.some(
-                  (element) => element.id === date.formatDate(time, 'x') / 1000
+                !pendetNotifs.notifications.some(
+                  (element) =>
+                    Number(element.id) === date.formatDate(time, 'x') / 1000
                 )
               ) {
                 notificationList.push({
                   title: task.title,
                   body: task.title,
-                  id: date.formatDate(time, 'x') / 1000,
+                  id:
+                    date.formatDate(time, 'x') / 1000 -
+                    (notification.notificationTimeValuesModel == 0 ? 1 : 0),
                   schedule: { at: time },
                   allowWhileIdle: true,
+                  autoCancel: false,
+                  actionTypeId: task.continuous
+                    ? 'CONTINUOUS_TASK_NOTIFICATION'
+                    : 'TASK_NOTIFICATION',
                   sound: null,
                   attachments: null,
-                  actionTypeId: '',
                   extra: null
                 })
               }
@@ -108,10 +181,23 @@ const actions = {
       notifications: notificationList
     })
     console.log(notifs)
-    // console.log(
-    //   await capacitor.Plugins.LocalNotifications.getPending()
-    // )
+    // dispatch('stopLocalPush')
   }
+  // async stopLocalPush({ getters }) {
+  //   try {
+  //     getters.capacitor.Plugins.LocalNotifications.getPending().then(
+  //       (res) => {
+  //         //console.log(res, "elo");
+  //         getters.capacitor.Plugins.LocalNotifications.cancel(res)
+  //       },
+  //       (error) => {
+  //         console.log(error)
+  //       }
+  //     )
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 }
 
 const getters = {
