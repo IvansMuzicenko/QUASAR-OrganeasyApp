0687ea747207caf9a0dd1b9d23f24e20d2227174
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-12-16 11:45:55 +0200

    feat: bottom action bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,108 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-footer elevated reveal class="bg-orange-3 text-black">
+      <div v-if="email" class="fixed-bottom text-center q-mb-xl q-pa-sm"></div>
+      <q-toolbar class="row no-padding">
         <q-btn
           flat
           dense
           round
-          icon="menu"
+          icon="more_vert"
           aria-label="Menu"
+          class="no-padding"
           @click="toggleLeftDrawer"
         />
+        <div class="col-11">
+          <q-list class="row">
+            <q-item
+              class="col justify-center no-padding"
+              clickable
+              exact
+              to="/"
+            >
+              <q-item-section avatar class="no-padding">
+                <q-avatar icon="view_list"></q-avatar>
+              </q-item-section>
+            </q-item>
+            <q-item
+              class="col justify-center no-padding"
+              clickable
+              exact
+              to="/notes"
+            >
+              <q-item-section avatar class="no-padding">
+                <q-avatar icon="note"></q-avatar>
+              </q-item-section>
+            </q-item>
+            <q-item class="col justify-center no-padding" clickable>
+              <q-item-section avatar class="no-padding">
+                <q-avatar
+                  round
+                  color="orange"
+                  text-color="white"
+                  icon="add"
+                ></q-avatar>
+              </q-item-section>
 
-        <q-toolbar-title> Organizify </q-toolbar-title>
+              <q-menu
+                class="text-center"
+                auto-close
+                self="top middle"
+                anchor="bottom middle"
+              >
+                <q-list style="min-width: 100px">
+                  <q-item clickable @click="addTask">
+                    <q-item-section avatar>
+                      <q-avatar icon="view_list"></q-avatar>
+                    </q-item-section>
+                    <q-item-section> Task</q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable @click="addNote">
+                    <q-item-section avatar>
+                      <q-avatar icon="note"></q-avatar>
+                    </q-item-section>
+                    <q-item-section> Note</q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable @click="addProcess">
+                    <q-item-section avatar>
+                      <q-icon name="update"></q-icon>
+                    </q-item-section>
+                    <q-item-section> Process</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-item>
+            <q-item
+              class="col justify-center no-padding"
+              clickable
+              exact
+              to="/processes"
+            >
+              <q-item-section avatar class="no-padding">
+                <q-icon name="update"></q-icon>
+              </q-item-section>
+            </q-item>
+            <q-item
+              class="col justify-center no-padding"
+              clickable
+              exact
+              to="/settings"
+            >
+              <q-item-section avatar class="no-padding">
+                <q-icon name="settings"></q-icon>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
       </q-toolbar>
-    </q-header>
+    </q-footer>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-toolbar-title>
+        <div>Organizify</div>
+      </q-toolbar-title>
       <q-list>
         <q-item-label v-if="email" header>
           {{ email.slice(0, email.indexOf('@')) }}</q-item-label
@@ -34,27 +121,31 @@
           :key="link.title"
           v-bind="link"
         />
-        <q-btn
-          v-if="email"
-          class="q-ma-sm"
-          color="primary"
-          label="+ add task"
-          @click="addTask"
-        />
-        <q-btn
-          v-if="email"
-          class="q-ma-sm"
-          color="secondary"
-          label="+ add process"
-          @click="addProcess"
-        />
-        <q-btn
-          v-if="email"
-          class="q-ma-sm"
-          color="secondary"
-          label="+ add note"
-          @click="addNote"
-        />
+        <div v-if="email" class="text-center">
+          <q-btn color="primary" rounded icon="add">
+            Add...
+            <q-menu
+              class="text-center"
+              auto-close
+              self="top middle"
+              anchor="bottom middle"
+            >
+              <q-list style="min-width: 100px">
+                <q-item clickable @click="addTask">
+                  <q-item-section>Task</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable @click="addNote">
+                  <q-item-section>Note</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable @click="addProcess">
+                  <q-item-section>Process</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
       </q-list>
     </q-drawer>
 
