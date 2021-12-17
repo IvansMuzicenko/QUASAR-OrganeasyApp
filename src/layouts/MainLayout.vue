11ad0e75f89de0704200b31c503dca87c29b3e7f
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-12-17 14:01:32 +0200

    style: bottom bar font size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-footer elevated reveal class="lt-sm bg-orange-3 text-black">
+    <q-footer elevated reveal class="lt-sm bg-orange-3 text-black text-caption">
       <div v-if="email" class="fixed-bottom text-center q-mb-xl q-pa-sm"></div>
       <q-toolbar class="row no-padding">
         <div class="col-12">
