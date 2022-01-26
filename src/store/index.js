3765678e28aaa1b4c4c1316faa27ec843ff5888f
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2022-01-26 13:09:27 +0200

    feat: categories store added

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { store } from 'quasar/wrappers'
 import { createStore } from 'vuex'
 import users from './users'
 import notification from './notification'
+import categories from './categories'
 
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
       users,
-      notification
+      notification,
+      categories
     },
     strict: process.env.DEBUGGING
   })
