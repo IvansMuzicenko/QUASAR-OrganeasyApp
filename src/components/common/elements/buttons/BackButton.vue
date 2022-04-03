c7b1212f0ba0870a5e3997f777af72c1cf85039e
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-04-03 12:07:35 +0300

    refactor: format code

--- src/components/common/elements/buttons/BackButton.vue
+++ src/components/common/elements/buttons/BackButton.vue
@@ -7,18 +7,17 @@ export default {
   methods: {
     goBack() {
       const routePath = this.$route.path
-      if (Object.keys(this.$route.query).length) {
-        this.$router.push(routePath)
-      } else {
-        const newPath = routePath.slice(0, routePath.lastIndexOf('/'))
-        if (
-          newPath.indexOf('/') == newPath.lastIndexOf('/') &&
-          /\d/.test(newPath)
-        ) {
-          return this.$router.push(`/?date=${newPath.replace('/', '')}`)
-        }
-        this.$router.push(newPath || '/')
-      }
+      if (Object.keys(this.$route.query).length)
+        return this.$router.push(routePath)
+
+      const newPath = routePath.slice(0, routePath.lastIndexOf('/'))
+      if (
+        newPath.indexOf('/') == newPath.lastIndexOf('/') &&
+        /\d/.test(newPath)
+      )
+        return this.$router.push(`/?date=${newPath.replace('/', '')}`)
+
+      this.$router.push(newPath || '/')
     }
   }
 }
